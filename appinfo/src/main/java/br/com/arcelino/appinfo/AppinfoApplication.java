43c36679aeff8cc044e76bf20414357e7d446fd5
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2024-05-23 17:24:22 -0300

    Importação Projeto Base

---
+++ appinfo/src/main/java/br/com/arcelino/appinfo/AppinfoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.appinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppinfoApplication.class, args);
+	}
+
+}
